50795aaed6c57ebb922fd80e039a628e73219d69
QuangHieu <nguyenquanghieu1704@gmail.com>
2023-08-30 23:44:37 +0700

    First commit for 3 challenges

---
+++ 1_hello_world.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+
+// g++ .\hello_world.cpp -o hello_world
+int main()
+{
+    cout << "Hello World!";
+    return 0;
+}
---
+++ 2_input_and_output.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int a;
+    int b;
+    int c;
+    int sum;
+    
+    cin >> a >> b >> c;
+    sum = a + b + c;
+    cout << sum;
+    
+    return 0;
+}
